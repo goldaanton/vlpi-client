2f7da62b584799de31b352ba26440a0f5bc72e4c
anton <goldaanton@gmail.com>
2021-12-09 00:12:15 +0200

    implement exercise passing flow

--- src/app/pages/tasks/tasks.component.ts
+++ src/app/pages/tasks/tasks.component.ts
@@ -1,8 +1,9 @@
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { DialogService } from 'src/app/dialog.service';
 import { ModulesService } from 'src/app/services/modules.service';
 
 @Component({
@@ -17,17 +18,23 @@ export class TasksComponent implements OnInit, OnDestroy {
 
   public tasks!: any[];
   public task!: any;
-  public activeTaskId!: string;
+  public activeTaskId!: number;
+
+  public isLastTask: boolean = false;
+  public answeredTasksIds: number[] = [];
 
   public answerBlocks: string[] = [];
   public userAnswerBlocks: string[] = [];
+  public correctBlocks: string[] = [];
 
   private tasksSubscription!: Subscription;
 
   constructor(
     private modulesService: ModulesService,
     private snackBar: MatSnackBar,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private dialogService: DialogService
   ) { }
 
   ngOnInit(): void {
@@ -65,8 +72,8 @@ export class TasksComponent implements OnInit, OnDestroy {
     }
   }
 
-  public setTask(taskId: string) {
-    this.task = this.tasks.find(task => task.id == taskId);
+  public setTask(taskId: number) {
+    this.task = this.tasks.find((task) => task.id == taskId);
     this.activeTaskId = this.task.id;
     this.answerBlocks = this.task.solutionBlocks.map((block: { text: any; }) => block.text );
     this.userAnswerBlocks = [];
@@ -95,8 +102,38 @@ export class TasksComponent implements OnInit, OnDestroy {
         this.snackBar.open('Something went wrong. Look in the console for details.', '' , {
           duration: 3000
         });
+      }, () => {
+        this.answeredTasksIds.push(this.activeTaskId);
       }
     );
+  }
+
+  public onNextTask(): void {
+    let index = this.tasks.indexOf(this.task);
+    if (this.tasks.length - index == 2) {
+      this.isLastTask = true;
+    }
+
+    this.setTask(this.tasks[index + 1].id);
+  }
+
+  public onShowAnswer(): void {
+
+  }
+
+  public onFinishExercise(): void {
+    this.dialogService.openConfirmDialog("Are you sure you want to finish this exercise?")
+      .afterClosed().subscribe(
+        (response) => {
+          if (response) {
+            this.router.navigate(['../../'], { relativeTo: this.activatedRoute });
+          }
+        }
+      )
+  }
+
+  public isTaskAnswered(): boolean {
+    return this.answeredTasksIds.includes(this.activeTaskId);
   }
 
   ngOnDestroy(): void {
