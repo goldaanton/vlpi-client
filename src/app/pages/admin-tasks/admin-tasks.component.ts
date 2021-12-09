3628f909f267aa512f1d7fb3b230638289f64ab6
anton <goldaanton@gmail.com>
2021-12-09 21:04:48 +0200

    refactor AdminTasksComponent

--- src/app/pages/admin-tasks/admin-tasks.component.ts
+++ src/app/pages/admin-tasks/admin-tasks.component.ts
@@ -1,11 +1,12 @@
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { SolutionBlock } from 'src/app/models';
 import { DialogService } from 'src/app/services/dialog.service';
 import { ModulesService } from 'src/app/services/modules.service';
+import { SnackBarService } from 'src/app/services/snack-bar.service';
 
 @Component({
   selector: 'app-admin-tasks',
@@ -15,27 +16,29 @@ import { ModulesService } from 'src/app/services/modules.service';
 export class AdminTasksComponent implements OnInit, OnDestroy {
 
   public exerciseId!: string;
+
   public tasks!: any[];
-  public task!: any;
+
+  public activeTask!: any;
   public activeTaskId!: string;
 
   public answerBlocks: string[] = [];
   public correctAnswerBlocks: string[] = [];
 
   public question!: string;
-  public points: number = 3;
+  public points: number = 100;
 
-  private tasksSubscription: Subscription | undefined;
+  public newBlockForm: FormGroup = new FormGroup({
+    text: new FormControl('')
+  });
 
-  newBlockForm: FormGroup = new FormGroup({
-    text: new FormControl('', Validators.required),
-  })
+  private tasksSubscription!: Subscription;
 
   constructor(
     private modulesService: ModulesService,
     private dialogService: DialogService,
     private activatedRoute: ActivatedRoute,
-    private snackBar: MatSnackBar
+    private snackService: SnackBarService
   ) { }
 
   ngOnInit(): void {
@@ -43,6 +46,74 @@ export class AdminTasksComponent implements OnInit, OnDestroy {
       this.exerciseId = params.get('id') || '1';
       this.fetchTasks(0);
     });
+  }
+
+  public setTask(taskId: string) {
+    this.activeTask = this.tasks.find(task => task.id == taskId);
+    this.activeTaskId = this.activeTask.id;
+    this.answerBlocks = this.activeTask.solutionBlocks
+      .map((block: SolutionBlock) => block.text );
+    this.correctAnswerBlocks = [];
+  }
+
+  // * NOTE: creating task only on front end to fill data
+  public onCreate() {
+    let index = (Math.max.apply(null, this.tasks.map((task) => parseInt(task.id))) + 1).toString();
+    this.tasks.push({
+      id: index,
+      question: '',
+      score: 0,
+      solutionBlocks: []
+    });
+    this.setTask(index);
+  }
+
+  // * NOTE: actual crating task in databse
+  public createTask(answerBlocksList: string[], correctAnswerList: string[]) {
+    let solutionBlocks: any[] = [];
+
+    answerBlocksList.forEach((text: string) => {
+      solutionBlocks.push({ text: text });
+    });
+
+    correctAnswerList.forEach((text: string, i: number) => {
+      solutionBlocks.push({
+        text: text,
+        solutionOrder: i
+      });
+    });
+
+    this.modulesService.createTask(
+      {
+        question: this.question,
+        score: this.points,
+        exerciseId: this.exerciseId,
+        solutionBlocks
+      }
+    ).subscribe(
+      (data) => {
+        this.snackService.showMessage('Task was created successfully!');
+        this.fetchTasks(this.tasks.length - 1);
+      }, (err) => {
+        this.snackService.showError(err);
+      }
+    );
+  }
+
+  public onDelete(exercise_id: string) {
+    this.dialogService.openConfirmDialog('Are you sure you want to delete this task?')
+      .afterClosed().subscribe((response) => {
+        if (response) {
+          alert(`Exercise with id ${exercise_id} was deleted`);
+        }
+      });
+  }
+
+  public addNewBlock() {
+    if (this.newBlockForm.value.text) {
+      this.answerBlocks.push(this.newBlockForm.value.text);
+      this.newBlockForm.reset();
+    }
   }
 
   public drop(event: CdkDragDrop<string[]>) {
@@ -58,99 +129,21 @@ export class AdminTasksComponent implements OnInit, OnDestroy {
     }
   }
 
-  public setTask(taskId: string) {
-    this.task = this.tasks.find(task => task.id == taskId);
-    this.activeTaskId = this.task.id;
-    this.answerBlocks = this.task.solutionBlocks.map((block: { text: any; }) => block.text );
-    this.correctAnswerBlocks = [];
-  }
-
-  public createTask(answerBlocksList: string[], correctAnswerList: string[]) {
-    let question = this.question;
-    let score = this.points;
-    let exerciseId = this.exerciseId;
-    let solutionBlocks: any[] = [];
-
-    answerBlocksList.forEach((text: string) => {
-      solutionBlocks.push({
-        text: text
-      });
-    });
-
-    correctAnswerList.forEach((text: string, i: number) => {
-      solutionBlocks.push({
-        text: text,
-        solutionOrder: i
-      });
-    });
-
-    this.modulesService.createTask(
-      {
-        question,
-        score,
-        exerciseId,
-        solutionBlocks
-      }
-    ).subscribe(
-      (data) => {
-        this.snackBar.open('Task was created successfully!', '', {
-          duration: 3000
-        });
-        this.fetchTasks(this.tasks.length - 1);
-      }, (err) => {
-        console.log(err);
-        this.snackBar.open('Something went wrong. Look in the console for details.', '' , {
-          duration: 3000
-        });
-      }
-    );
-  }
-
-  public onCreate() {
-    let index = (Math.max.apply(null, this.tasks.map((task) => parseInt(task.id))) + 1).toString();
-    this.tasks.push({
-      id: index,
-      question: '',
-      score: 0,
-      solutionBlocks: []
-    });
-    this.setTask(index);
-  }
-
-  public onDelete(exercise_id: string) {
-    this.dialogService.openConfirmDialog('Are you sure you want to delete this task?')
-      .afterClosed().subscribe((response) => {
-        if (response) {
-          alert(`Exercise with id ${exercise_id} was deleted`);
-        }
-      });
-  }
-
-  public addNewBlock() {
-    if (this.newBlockForm.valid) {
-      this.answerBlocks.push(this.newBlockForm.value.text);
-      this.newBlockForm.reset();
-    }
+  ngOnDestroy(): void {
+    this.tasksSubscription?.unsubscribe();
   }
 
   private fetchTasks(activeTaskId: number) {
-    this.tasksSubscription = this.modulesService.getTasks(this.exerciseId).subscribe(
-      (data) => {
-        this.tasks = data.tasks;
-        console.log(this.tasks);
-        console.log(activeTaskId);
-        this.setTask(this.tasks[activeTaskId].id);
-      }, (err) => {
-        console.log(err);
-        this.snackBar.open('Something went wrong. Look in the console for details.', '' , {
-          duration: 3000
-        });
-      }
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.tasksSubscription?.unsubscribe();
+    this.tasksSubscription = this.modulesService
+      .getTasks(this.exerciseId)
+      .subscribe(
+        (data) => {
+          this.tasks = data.tasks;
+          this.setTask(this.tasks[activeTaskId].id);
+        }, (err) => {
+          this.snackService.showError(err);
+        }
+      );
   }
 
 }
